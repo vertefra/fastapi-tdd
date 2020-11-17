d20bf4028408df41bb37acd1336fe8dc1f992c83
vertefra <verte.fra@gmail.com>
2020-11-17 15:02:09 -0500

    test post route

---
+++ project/tests/test_summaries.py
@@ -0,0 +1,11 @@
+import json
+
+import pytest
+
+
+def test_create_summary(test_app_with_db):
+    response = test_app_with_db.post(
+        "/summaries/", data=json.dumps({"url": "https://foo.bar"}))
+
+    assert response.status_code == 200
+    assert response.json()["url"] == "https://foo.bar"
